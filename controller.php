d074331d887042528860c1c9d7405d6acd82489d
tinydeal <295174028@qq.com>
2013-02-05 14:53:43 +0800

    权限bug

--- controller.php
+++ controller.php
@@ -14,20 +14,20 @@ if(isset($_GET['mod'])){
 			require_once './home_controller.php';
 			break;
 		case 'supplier_category':
-			if(strstr($_SESSION['user_permission_name'],CodeNames::permission_name_procurement)||strstr($_SESSION['user_permission_name'],CodeNames::permission_name_administrator))
+			if(strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_procurement)||strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_administrator))
 				require_once './supplier_category_controller.php';
 			else
 				header("Location: ./index.php?mod=home");
 			break;
 		case 'supplier':
-			if(strstr($_SESSION['user_permission_name'],CodeNames::permission_name_procurement)||strstr($_SESSION['user_permission_name'],CodeNames::permission_name_administrator))
+			if(strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_procurement)||strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_administrator))
 				require_once './supplier_controller.php';
 			else
 				header("Location: ./index.php?mod=home");
 			break;
 
 		case 'goods_category':
-			if(strstr($_SESSION['user_permission_name'],CodeNames::permission_name_procurement)||strstr($_SESSION['user_permission_name'],CodeNames::permission_name_administrator))
+			if(strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_procurement)||strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_administrator))
 				require_once './goods_category_controller.php';
 			else
 				header("Location: ./index.php?mod=home");
@@ -35,27 +35,47 @@ if(isset($_GET['mod'])){
 			
 		
 		case 'goods':
-			if(strstr($_SESSION['user_permission_name'],CodeNames::permission_name_procurement)||strstr($_SESSION['user_permission_name'],CodeNames::permission_name_administrator))
+			if(strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_procurement)||strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_administrator))
 				require_once './goods_controller.php';
 			else
 				header("Location: ./index.php?mod=home");
 			break;
 			
 		case 'order':
-			require_once './order_controller.php';
+			if(strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_order)||strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_administrator))
+				require_once './order_controller.php';
+			else
+				header("Location: ./index.php?mod=home");
 			break;
+
 		case 'problem':
-			require_once './problem_controller.php';
-			break;
+			//if(strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_order)||strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_administrator))
+				require_once './problem_controller.php';
+			//else
+			//	header("Location: ./index.php?mod=home");
+			break;		
+
 		case 'personnel_category':
-			require_once './personnel_category_controller.php';
-			break;
+			if(strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_administrator))
+				require_once './personnel_category_controller.php';
+			else
+				header("Location: ./index.php?mod=home");
+			break;			
+
 		case 'user':
-			require_once './user_controller.php';
+			//if(strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_administrator))
+				require_once './user_controller.php';
+			//else
+			//	header("Location: ./index.php?mod=home");
 			break;
+
 		case 'log':
-			require_once './log_controller.php';
+			if(strstr($_SESSION['user_permission_name'],CodeNames::$permission_name_administrator))
+				require_once './log_controller.php';
+			else
+				header("Location: ./index.php?mod=home");
 			break;
+
 		default:	
 			break;
 	}
